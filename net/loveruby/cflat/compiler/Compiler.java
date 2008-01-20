d04a86cce8fed7952cadc0f9290340600e3b277e
Minero Aoki <mineroaoki@gmail.com>
2008-01-20 20:28:53 +0000

    * net/loveruby/cflat/compiler/Compiler.java: new command line option -S (generate assembly source and quit). * net/loveruby/cflat/compiler/Compiler.java: new command line option -c (assemble and quit). * net/loveruby/cflat/compiler/Compiler.java: call GNU as and GNU ld directly, instead of gcc. * net/loveruby/cflat/compiler/Compiler.java (commandMain): refactoring: extract method: parseOptions. * net/loveruby/cflat/compiler/Compiler.java: refactoring: introduce new class Options and hold all command options in it. * net/loveruby/cflat/compiler/Compiler.java: refactoring: #compileFile do all tasks by referencing an Options.
    git-svn-id: file:///Users/aamine/c/gitwork/public/cbc/trunk@3856 1b9489fe-b721-0410-924e-b54b9192deb8

--- net/loveruby/cflat/compiler/Compiler.java
+++ net/loveruby/cflat/compiler/Compiler.java
@@ -8,63 +8,103 @@ import java.util.*;
 import java.io.*;
 
 public class Compiler {
+    static final private String programId = "cbc";
+
     static public void main(String[] args) {
-        new Compiler().commandMain(args);
+        new Compiler(programId).commandMain(args);
     }
 
-    static final private String programId = "cbc";
-    protected boolean debugParser;
-    protected TypeTable typeTable;
-    protected LibraryLoader loader;
     protected ErrorHandler errorHandler;
 
-    public Compiler() {
-        debugParser = false;
-        typeTable = TypeTable.ilp32();
-        loader = new LibraryLoader();
-        errorHandler = new ErrorHandler(programId);
+    public Compiler(String programName) {
+        this.errorHandler = new ErrorHandler(programName);
     }
 
-    public Compiler(TypeTable table, LibraryLoader ld, ErrorHandler h) {
-        debugParser = false;
-        typeTable = table;
-        loader = ld;
-        errorHandler = h;
+    private TypeTable defaultTypeTable() {
+        return TypeTable.ilp32();
     }
 
     public void commandMain(String[] origArgs) {
-        // parse options
-        String mode = null;
-        List args = listFromArray(origArgs);
+        Options opts = parseOptions(listFromArray(origArgs));
+        if (opts.isMode("--check-syntax")) {
+            if (isValidSyntax(opts)) {
+                System.out.println("Syntax OK");
+                System.exit(0);
+            }
+            else {
+                System.exit(1);
+            }
+        }
+        else {
+            try {
+                compileFile(opts);
+            }
+            catch (CompileException ex) {
+                errorHandler.error(ex.getMessage());
+                System.exit(1);
+            }
+        }
+    }
+
+    class Options {
+        public String mode;
+        public String inputFile;
+        //public String outputFile;
+        public boolean debugParser;
+        //public boolean debugBuild;
+        public TypeTable typeTable;
+        public LibraryLoader loader;
+        //public List ldArgs;   // List<LdArg>
+
+        public boolean isMode(String m) {
+            return mode == null ? false : mode.equals(m);
+        }
+    }
+
+    protected Options parseOptions(List args) {
         ListIterator it = args.listIterator();
+        Options opts = new Options();
+        opts.typeTable = defaultTypeTable();
+        opts.loader = new LibraryLoader();
         while (it.hasNext()) {
             String arg = (String)it.next();
             if (arg.startsWith("-")) {
-                if (arg.startsWith("-I")) {
-                    String path = arg.substring(2);
-                    if (path.length() == 0) {
-                        if (! it.hasNext()) {
-                            errorExit("-I option missing argument");
-                        }
-                        it.remove();
-                        path = (String)it.next();
-                    }
-                    loader.addLoadPath(path);
-                }
-                else if (arg.equals("--check-syntax")
+                if (arg.equals("--check-syntax")
                         || arg.equals("--dump-tokens")
                         || arg.equals("--dump-ast")
                         || arg.equals("--dump-reference")
-                        || arg.equals("--dump-semantic")) {
-                    if (mode != null) {
-                        errorExit(mode + " option and " +
-                                  arg + " option is exclusive");
+                        || arg.equals("--dump-semantic")
+                        || arg.equals("-S")
+                        || arg.equals("-c")) {
+                    if (opts.mode != null) {
+                        errorExit(opts.mode + " option and "
+                                  + arg + " option is exclusive");
                     }
-                    mode = arg;
+                    opts.mode = arg;
+                }
+                else if (arg.startsWith("-I")) {
+                    opts.loader.addLoadPath(getOptArg(arg, it));
                 }
                 else if (arg.equals("--debug-parser")) {
-                    debugParser = true;
+                    opts.debugParser = true;
                 }
+                // FIXME: generic options
+                //else if (arg.startsWith("-o"))
+                //else if (arg.equals("--type-model"))
+                // FIXME: compile options
+                //else if (arg.equals("-g"))
+                //else if (arg.equals("-fPIC"))
+                //else if (arg.startsWith("-O"))
+                // FIXME: assemble options
+                //else if (arg.startsWith("-Wa,"))
+                //else if (arg.equals("-Xassembler"))
+                // FIXME: link options
+                //else if (arg.equals("-static"))
+                //else if (arg.equals("-shared"))
+                //else if (arg.startsWith("-L"))
+                //else if (arg.startsWith("-l"))
+                //else if (arg.startsWith("-Wl,"))
+                //else if (arg.equals("-Xlinker"))
                 else if (arg.equals("--help")) {
                     printUsage(System.out);
                     System.exit(0);
@@ -77,46 +117,13 @@ public class Compiler {
                 it.remove();
             }
         }
-        if (mode == null) {
-            mode = "compile";
-        }
+
+        // FIXME: handle many input files
         if (args.size() == 0) errorExit("no input file");
         if (args.size() > 1) errorExit("too many input files");
-        String inputFile = (String)args.get(0);
+        opts.inputFile = (String)args.get(0);
 
-        // execute
-        try {
-            if (mode.equals("compile")) {
-                compileFile(inputFile);
-            }
-            else if (mode.equals("--check-syntax")) {
-                if (isValidSyntax(inputFile)) {
-                    System.out.println("Syntax OK");
-                    System.exit(0);
-                } else {
-                    System.exit(1);
-                }
-            }
-            else if (mode.equals("--dump-tokens")) {
-                dumpTokensFromFile(inputFile);
-            }
-            else if (mode.equals("--dump-ast")) {
-                dumpASTFromFile(inputFile);
-            }
-            else if (mode.equals("--dump-reference")) {
-                dumpReferenceFromFile(inputFile);
-            }
-            else if (mode.equals("--dump-semantic")) {
-                dumpSemanticFromFile(inputFile);
-            }
-            else {
-                throw new Error("unknown mode: " + mode);
-            }
-        }
-        catch (CompileException ex) {
-            errorHandler.error(ex.getMessage());
-            System.exit(1);
-        }
+        return opts;
     }
 
     protected void printUsage(PrintStream out) {
@@ -127,6 +134,9 @@ public class Compiler {
         out.println("  --dump-tokens    Parses source file and dumps tokens.");
         out.println("  --dump-ast       Parses source file and dumps AST.");
         out.println("  --dump-semantic  Check semantics and dumps AST.");
+        out.println("  -S               Generate assembly source only.");
+        out.println("  -c               Compile, Assemble and stop.");
+        out.println("  -I PATH          Add import file loading path.");
         out.println("  --help           Prints this message and quit.");
     }
 
@@ -138,14 +148,28 @@ public class Compiler {
         return list;
     }
 
+    private String getOptArg(String arg, ListIterator it) {
+        String path = arg.substring(2);
+        if (path.length() != 0) {       // -Ipath
+            return path;
+        }
+        else {                          // -I path
+            if (! it.hasNext()) {
+                errorExit("-I option missing argument");
+            }
+            it.remove();
+            return (String)it.next();
+        }
+    }
+
     private void errorExit(String msg) {
         errorHandler.error(msg);
         System.exit(1);
     }
 
-    public boolean isValidSyntax(String path) {
+    protected boolean isValidSyntax(Options opts) {
         try {
-            parseFile(path);
+            parseFile(opts);
             return true;
         }
         catch (SyntaxException ex) {
@@ -157,15 +181,31 @@ public class Compiler {
         }
     }
 
-    public AST parseFile(String path) throws SyntaxException, FileException {
-        return Parser.parseFile(new File(path), loader,
-                                errorHandler, debugParser);
-    }
-
-    public void dumpTokensFromFile(String path) throws CompileException {
-        AST ast = parseFile(path);
-        Token t = ast.firstToken();
-        dumpTokenList(t, System.out);
+    protected void compileFile(Options opts) throws CompileException {
+        AST ast = parseFile(opts);
+        if (opts.isMode("--dump-tokens")) {
+            dumpTokenList(ast.firstToken(), System.out);
+            return;
+        }
+        if (opts.isMode("--dump-ast")) {
+            ast.dump();
+            return;
+        }
+        semanticAnalysis(ast, opts);
+        if (opts.isMode("--dump-semantic")) {
+            ast.dump();
+            return;
+        }
+        String asm = CodeGenerator.generate(ast, opts.typeTable, errorHandler);
+        writeFile(asmFileName(opts.inputFile), asm);
+        if (opts.isMode("-S")) {
+            return;
+        }
+        assemble(asmFileName(opts.inputFile), objFileName(opts.inputFile));
+        if (opts.isMode("-c")) {
+            return;
+        }
+        link(objFileName(opts.inputFile), exeFileName(opts.inputFile));
     }
 
     protected void dumpTokenList(Token t, PrintStream s) {
@@ -187,53 +227,51 @@ public class Compiler {
         s.println(TextUtils.escapeString(t.image));
     }
 
-    public void dumpASTFromFile(String path) throws CompileException {
-        parseFile(path).dump();
+    protected AST parseFile(Options opts)
+                            throws SyntaxException, FileException {
+        return Parser.parseFile(new File(opts.inputFile),
+                                opts.loader,
+                                errorHandler,
+                                opts.debugParser);
     }
 
-    public void dumpReferenceFromFile(String path) throws CompileException {
-        AST ast = parseFile(path);
-        TypeTable typeTable = defaultTypeTable();
+    protected void semanticAnalysis(AST ast, Options opts)
+                                        throws SemanticException {
         JumpResolver.resolve(ast, errorHandler);
         LocalReferenceResolver.resolve(ast, errorHandler);
-        TypeResolver.resolve(ast, typeTable, errorHandler);
-        typeTable.semanticCheck(errorHandler);
+        TypeResolver.resolve(ast, opts.typeTable, errorHandler);
+        opts.typeTable.semanticCheck(errorHandler);
         DereferenceChecker.check(ast, errorHandler);
-        ast.dump();
+        if (opts.isMode("--dump-reference")) {
+            ast.dump();
+            System.exit(1);
+        }
+        TypeChecker.check(ast, opts.typeTable, errorHandler);
     }
 
-    public void dumpSemanticFromFile(String path) throws CompileException {
-        AST ast = parseFile(path);
-        TypeTable typeTable = defaultTypeTable();
-        semanticAnalysis(ast, typeTable);
-        ast.dump();
+    protected void assemble(String srcPath,
+                            String destPath) throws IPCException {
+        String[] cmd = {
+            "as",
+            "-o", destPath,
+            srcPath
+        };
+        invoke(cmd);
     }
 
-    public void compileFile(String path) throws CompileException {
-        AST ast = parseFile(path);
-        TypeTable typeTable = defaultTypeTable();
-        semanticAnalysis(ast, typeTable);
-        String asm = CodeGenerator.generate(ast, typeTable, errorHandler);
-        writeFile(asmFileName(path), asm);
-        assemble(asmFileName(path));
-    }
-
-    public void semanticAnalysis(AST ast, TypeTable typeTable)
-                                            throws SemanticException {
-        JumpResolver.resolve(ast, errorHandler);
-        LocalReferenceResolver.resolve(ast, errorHandler);
-        TypeResolver.resolve(ast, typeTable, errorHandler);
-        typeTable.semanticCheck(errorHandler);
-        DereferenceChecker.check(ast, errorHandler);
-        TypeChecker.check(ast, typeTable, errorHandler);
-    }
-
-    private TypeTable defaultTypeTable() {
-        return TypeTable.ilp32();
-    }
-
-    public void assemble(String path) throws IPCException {
-        String[] cmd = {"gcc", path, "-o", cmdFileName(path)};
+    protected void link(String srcPath,
+                        String destPath) throws IPCException {
+        String[] cmd = {
+            "ld",
+            "-dynamic-linker", "/lib/ld-linux.so.2",
+            "/usr/lib/crt1.o",
+            "/usr/lib/crti.o",
+            srcPath,
+            "/usr/lib/libc_nonshared.a",
+            "-lc",
+            "/usr/lib/crtn.o",
+            "-o", destPath
+        };
         invoke(cmd);
     }
 
@@ -244,8 +282,8 @@ public class Compiler {
             passThrough(proc.getInputStream());
             passThrough(proc.getErrorStream());
             if (proc.exitValue() != 0) {
-                errorHandler.error("gcc failed (assemble); status " +
-                              proc.exitValue());
+                errorHandler.error(cmd[0] + " failed."
+                                   + " (status " + proc.exitValue() + ")");
                 throw new IPCException("compile error");
             }
         }
@@ -293,7 +331,11 @@ public class Compiler {
         return baseName(orgPath, true) + ".s";
     }
 
-    protected String cmdFileName(String orgPath) {
+    protected String objFileName(String orgPath) {
+        return baseName(orgPath, true) + ".o";
+    }
+
+    protected String exeFileName(String orgPath) {
         return baseName(orgPath, true);
     }
 
